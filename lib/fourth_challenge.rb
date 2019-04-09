f198f6a2e571de149ad993c4a28eca100811ae8f
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 00:56:21 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,6 +10,11 @@ def fourth_challenge
           name: "Benvolio",
           age: "17",
           attitude: "worried"
+        },
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
         }
       ]
    }, 
@@ -19,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
       ]
    }
 }
